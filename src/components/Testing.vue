30d2f39f605ccc975d8dc670604f19296a69f113
Isidro GARCIA GARCIA <isidro.gg@gmail.com>
2021-05-07 20:48:45 +0200

    Testing

--- src/components/Testing.vue
+++ src/components/Testing.vue
@@ -1,8 +1,8 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="container mx-auto flex justify-center">
-    <div class="mt-32 w-2/4">
-      <form @submit.prevent="login" v-if="!isLogged">
+    <div class="mt-32 w-2/4" v-if="!isLogged">
+      <form @submit.prevent="login">
         <div class="shadow overflow-hidden sm:rounded-md">
           <div class="px-4 py-5 bg-white sm:p-6">
             <div class="col-span-6 sm:col-span-3 mt-6">
@@ -25,6 +25,7 @@
             </div>
           </div>
           <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+
             <button
                 type="submit"
                 class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -33,18 +34,38 @@
           </div>
         </div>
       </form>
-      <div v-else>
-        <h1>Usuario logueado</h1>
+    </div>
+    <div v-else class="flex flex-wrap mt-6">
+      <div v-for="game in games" class="flex items-center justify-center w-1/6 mt-2">
+        <div class="space-y-8 bg-white p-4">
+          <div>
+            <img class="mx-auto h-12 w-auto" :src="game.thumbnailUrl" alt="Workflow" />
+            <p class="mt-6 text-center">
+              { game.title }
+            </p>
+            <p class="mt-2 text-center text-sm text-gray-600">
+              Or
+              {{ ' ' }}
+              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+            </span>
+            Sign in
+          </button>
+        </div>
       </div>
-      <pre>
-        {{ game }}
-      </pre>
     </div>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue'
 import { ref, reactive } from 'vue'
+
 const users = [
   {
     username: 'usuario1',
@@ -70,34 +91,16 @@ const infoUser = reactive({
 const games = ref([]);
 
 
-const fetchData = async () => {
-    return fetch('https://www.freetogame.com/api/games?category=racing', {
-      method: 'get',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-        .then(res => {
-          return res.json();
-        })
-        .then(json => {
-          console.log('hola');
-          console.log(json, 'aaaaaaa');
-          console.log(json.data, 'aaaaaaa');
-        })
-        .catch(err => {
-          if (err.json) {
-            return err.json.then(json => {
-            });
-          }
-        })
-        .then(() => {
-        });
+const fetchData = () => {
+  fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+      .then(response => response.json())
+      .then(json => games.value = json)
 }
 
 
 
 export default {
+  components: {Card},
   setup() {
     const login = () => {
       if(users.some(item => item.username === infoUser.username && item.password === infoUser.password)) {
@@ -111,7 +114,8 @@ export default {
     return {
       login,
       infoUser,
-      isLogged
+      isLogged,
+      games
     }
   },
 }
